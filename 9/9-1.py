f1999ad445e7a46dfe2c268afda2eb7fe6fb4fd9
lesvay <lesvay@qq.com>
2020-11-08 09:21:44 +0800

    202011080921

---
+++ 9/9-1.py
@@ -0,0 +1,60 @@
+from cv2 import cv2
+import numpy as np
+img=cv2.imread('9/9-1.jpg')
+'''
+#获取灰度值和修改灰度值
+print(img)#输出该图像的灰度值
+px=img[100,100]
+print(px)#输出100行100列像素的rgb
+img[100,100]=[0,0,0]#改变第100行100列像素的rgb
+blue=img[100,100,0]
+print(blue)#img[100,100]第0位，即rgb中的r
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+'''
+'''
+print(img.shape)#形状
+print(img.size)#像素数目
+print(img.dtype)#数据类型
+'''
+'''
+#ROI
+copy=img[280:340,330:390]
+img[273:333,100:160]=copy
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+'''
+
+'''
+拆分rgb通道
+b,g,r=cv2.split(img)
+
+#类似
+# b=img[:,:,0]
+# g=img[:,:,1]
+# r=img[:,:,2]
+
+# cv2.imshow('image',g)
+# cv2.imshow('image',b)
+cv2.imshow('image',r)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+'''
+'''
+#去除通道
+#openCV按照BGR排序，matplotlib按照RGB排序
+img[:,:,0]=0 #去除b蓝色通道
+img[:,:,1]=0 #去除g绿色通道
+img[:,:,2]=0 #去除r红色通道
+img[:,:,:]=0 #去除全部通道，即变成了黑色
+'''
+'''
+#通道合并
+b,g,r=cv2.split(img)
+img=cv2.merge([b,g,r])#加上[]
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+'''
---
+++ 9/9-2.py
@@ -0,0 +1,20 @@
+from cv2 import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+BLUE=[255,0,0]
+img1=cv2.imread('9/9-1.jpg')
+
+replicate  = cv2.copyMakeBorder(img1,10,10,10,10,cv2.BORDER_REPLICATE)
+reflect    = cv2.copyMakeBorder(img1,10,10,10,10,cv2.BORDER_REFLECT)
+reflect101 = cv2.copyMakeBorder(img1,10,10,10,10,cv2.BORDER_REFLECT_101)
+wrap       = cv2.copyMakeBorder(img1,10,10,10,10,cv2.BORDER_WRAP)
+constant   = cv2.copyMakeBorder(img1,10,10,10,10,cv2.BORDER_CONSTANT,value=BLUE)
+
+#subplot(231)2行3列第1个
+plt.subplot(231),plt.imshow(img1,'gray'),plt.title('ORIGINAL')
+plt.subplot(232),plt.imshow(replicate,'gray'),plt.title('REPLICATE')#复制
+plt.subplot(233),plt.imshow(reflect,'gray'),plt.title('REFLECT')#映射
+plt.subplot(234),plt.imshow(reflect101,'gray'),plt.title('REFLECT_101')
+plt.subplot(235),plt.imshow(wrap,'gray'),plt.title('WRAP')#包裹
+plt.subplot(236),plt.imshow(constant,'gray'),plt.title('CONSTANT')#连续
+plt.show()
